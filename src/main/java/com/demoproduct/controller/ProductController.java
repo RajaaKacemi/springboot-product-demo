a0cb3e296cc0491f1fe64edeeb7569addceb55b2
SihamYoubi <youbisiham5@gmail.com>
2025-10-12 17:26:50 +0100

    add delete and add products methods

--- src/main/java/com/demoproduct/controller/ProductController.java
+++ src/main/java/com/demoproduct/controller/ProductController.java
@@ -3,10 +3,14 @@ package com.demoproduct.controller;
 
 import com.demoproduct.entities.Product;
 import com.demoproduct.repository.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.util.List;
@@ -26,9 +30,25 @@ public class ProductController {
        return "products";
     }
 
-    @DeleteMapping("/delete")
-    public String deleteProduct(Long id){
+    @GetMapping("/delete")
+    public String deleteProduct(@RequestParam Long id){
         productRepository.deleteById(id);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/addProduct")
+    public String addProduct(Product product, Model model){
+        model.addAttribute(product);
+        return "new-product";
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product , BindingResult bindingResult , Model model){
+        if(bindingResult.hasErrors()){
+//            model.addAttribute("Recheck the informations");
+            return "new-product";
+        }
+        productRepository.save(product);
         return "redirect:/products";
     }
 
